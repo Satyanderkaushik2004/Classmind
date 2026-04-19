1c2ca77c31c89ac64314c2bcda4b1472dbb4049f
Satyanderkaushik2004 <kaushikkrish1999@gmail.com>
2026-04-19 13:46:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -624,8 +624,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://satyanderkaushik2004.github.io"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
